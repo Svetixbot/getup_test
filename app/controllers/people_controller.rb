741a9949b53533d33f6f188a8b2afa6d20c01082
Svetlana Filimonova <sfilimon@hotmail.com>
2011-06-19 15:05:08 +1000

    Modify controller and view to show data from DB

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,9 @@
 class PeopleController < ApplicationController
+  def show
+    @people = Person.all
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @people }
+    end
+  end
 end
