89847d4fb6694723eed85f99b444268ee0d1d23a
Svetlana Filimonova <sfilimon@hotmail.com>
2011-06-19 16:04:44 +1000

    Acceptance tests for filtering. Fail for now.

--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -11,5 +11,32 @@ class PeopleControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:people)
   end
+
+# Acceptance tests from GetUp task (look at README) 
+
+  test "should get people with postcodes" do
+    post(:show , {'include_postcodes' => '2000,2010'})
+    assert_equal(4, assigns[:people].count)
+  end
+  
+  test "should get people by email domain" do
+    post(:show , {'include_email' => 'hotmail'})
+    assert_equal(2, assigns[:people].count)
+  end
+  
+  test "should get people exclude with postcode" do
+    post(:show , {'exclude_postcodes' => '2000'})
+    assert_equal(8, assigns[:people].count)
+  end
+  
+  test "should get people with postcode and email domain" do
+    post(:show , {'include_postcodes' => '2010', 'include_email' => 'gmail'})
+    assert_equal(1, assigns[:people].count)
+  end  
+  
+  test "should get people with poscodes exclude email domain" do
+    post(:show , {'include_postcodes' => '2060, 2061, 2065', 'exclude_email' => 'gmail'})
+    assert_equal(1, assigns[:people].count)
+  end
   
 end
